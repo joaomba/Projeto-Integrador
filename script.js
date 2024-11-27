a91d906cabc5d406823fdf2cbc3d50557226177e
joaomba <jpbrflow@gmail.com>
2024-11-27 19:20:50 -0300

    site

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ $(document).ready(function() {
         $('#mobile_btn').find('i').toggleClass('fa-x'); 
     });
 });
+const estrelas = document.querySelectorAll('.estrela');
+const resultado = document.querySelector('.resultado');
+
+estrelas.forEach(estrela => {
+    estrela.addEventListener('click', () => {
+        const valor = estrela.getAttribute('data-valor');
+
+        estrelas.forEach(e => e.classList.remove('checked'));
+
+        
+        for (let i = 0; i < valor; i++) {
+            estrelas[i].classList.add('checked');
+        }
+
+        resultado.textContent = `Você avaliou com ${valor} estrela(s)`;
+    });
+});
